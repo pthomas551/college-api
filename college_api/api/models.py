5d360e117ae9bb30dc092e0f02173843da4fe442
Phil Thomas <pthomas551@gmail.com>
2016-06-14 18:04:36 -0500

    edit relationships

--- college_api/api/models.py
+++ college_api/api/models.py
@@ -3,16 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class User(models.Model):
-    name = models.CharField(max_length=60)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ('name',)
-
-
 class Attribute(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=60)
@@ -27,8 +17,18 @@ class Attribute(models.Model):
 class College(models.Model):
     name = models.CharField(max_length=100)
     size = models.IntegerField()
-    attributes = models.ManyToManyField(Attribute)
-    users = models.ManyToManyField(User)
+    attributes = models.ManyToManyField(Attribute, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=60)
+    colleges = models.ManyToManyField(College, blank=True)
 
     def __str__(self):
         return self.name
--- college_api/api/serializers.py
+++ college_api/api/serializers.py
@@ -1,14 +1,30 @@
 from rest_framework import serializers
-from api.models import User, Attribute, College
-
-
-class CollegeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = College
-        fields = ('id', 'name', 'size')
+from api.models import Attribute, College
 
 
 class AttributeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attribute
         fields = ('id', 'name', 'category')
+
+
+class CollegeSerializer(serializers.ModelSerializer):
+    attributes = AttributeSerializer(many=True)
+
+    class Meta:
+        model = College
+        fields = ('id', 'name', 'size', 'attributes')
+
+    def create(self, validated_data):
+        attributes_data = validated_data.pop('attributes')
+        college = College.objects.create(**validated_data)
+        for attribute_data in attributes_data:
+            Attribute.objects.create(college=college, **attribute_data)
+        return college
+
+    def update(self, validated_data):
+        attributes_data = validated_data.pop('attributes')
+        college = College.objects.update(**validated_data)
+        for attribute_data in attributes_data:
+            Attribute.objects.update(college=college, **attribute_data)
+        return college
