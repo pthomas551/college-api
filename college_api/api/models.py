bf707417a9882953875785a09a9cac0c97516e51
Phil Thomas <pthomas551@gmail.com>
2016-06-13 22:14:19 -0500

    add models

--- college_api/api/models.py
+++ college_api/api/models.py
@@ -2,4 +2,36 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class College(models.Model):
+    name = models.CharField(max_length=100)
+    size = models.IntegerField()
+    attributes = models.ManyToManyField(Attribute)
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
